79917dc876c97b34f38144e0cc2760bef7df208b
Michał Siwek <mike21@aol.pl>
2019-08-20 03:41:44 +0200

    Return the tasks ordered by list_position first

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,7 +5,7 @@ class TasksController < ApiController
   param :task_list_id, :number
   param :page, :number, 'Starts from 1. Will return items in bulks of 25, starting from the most recent.'
   def index
-    render json: task_list.tasks.order('id asc').page(params[:page])
+    render json: task_list.tasks.order('list_position asc, id asc').page(params[:page])
   end
 
   api :GET, '/task_lists/:task_list_id/tasks/:id', 'Returns requested task details'
