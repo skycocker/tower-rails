2fb4b6f59050f9bfc1b2bc71638251e35ce1dc3a
Michał Siwek <mike21@aol.pl>
2019-08-20 03:03:04 +0200

    Add in the default ORM for devise_token_auth

--- config/initializers/devise_token_auth.rb
+++ config/initializers/devise_token_auth.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 DeviseTokenAuth.setup do |config|
+  require 'devise/orm/active_record'
   # By default the authorization headers will change after each request. The
   # client is responsible for keeping track of the changing tokens. Change
   # this to false to prevent the Authorization header from changing after
