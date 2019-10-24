f5fc6ec3ce4271db19d0b1928a8b2679d2801f43
Michał Siwek <mike21@aol.pl>
2019-10-24 22:26:58 +0200

    Implement basic push notification interface

---
+++ app/lib/push_notification.rb
@@ -0,0 +1,25 @@
+class PushNotification
+  attr_reader :fcm_server_key, :users, :title, :content
+
+  def initialize(fcm_server_key:, users:, title:, content:)
+    @users   = users
+    @title   = title
+    @content = content
+    @fcm_server_key = fcm_server_key
+  end
+
+  def send
+    fcm.send(users.pluck(:fcm_token), {
+      notification: {
+        title: title,
+        body:  content,
+      },
+    })
+  end
+
+  private
+
+  def fcm
+    @fcm = FCM.new(fcm_server_key)
+  end
+end
