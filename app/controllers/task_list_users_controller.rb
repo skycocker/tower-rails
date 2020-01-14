dba02ee52da681ad3f17a9d223ae8068f4056ef2
Michał Siwek <mike21@aol.pl>
2020-01-14 16:34:55 +0100

    Create POST /task_lists/:task_list_id/task_list_users endpoint

--- app/controllers/task_list_users_controller.rb
+++ app/controllers/task_list_users_controller.rb
@@ -4,6 +4,25 @@ class TaskListUsersController < ApiController
   api :GET, '/task_lists/:task_list_id/task_list_users', 'Returns users associated to the requested task list'
   param :task_list_id, :number
   def index
-    render json: current_user.task_lists.find(params[:task_list_id]).users
+    render json: task_list.users
+  end
+
+  api :POST, '/task_lists/:task_list_id/task_list_users', 'Adds a new user to the list (using a provided email address)'
+  param :task_list_id, :number
+  param :email, String
+  def create
+    new_user = task_list.users.find_or_initialize_by(email: params[:email])
+
+    if new_user.save
+      render json: new_user, status: :created
+    else
+      render json: { errors: new_user.errors }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def task_list
+    current_user.task_lists.find(params[:task_list_id])
   end
 end
