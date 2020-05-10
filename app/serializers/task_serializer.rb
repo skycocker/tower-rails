40652f2ec06b7c0131e839ddecb682b064e20a98
Michał Siwek <mike21@aol.pl>
2020-05-10 19:04:59 +0200

    Return the unified task note presence flag for the Task model

--- app/serializers/task_serializer.rb
+++ app/serializers/task_serializer.rb
@@ -1,11 +1,13 @@
 class TaskSerializer < ActiveModel::Serializer
   attributes :id, :task_list_id, :content,
-             :list_position, :task_note_count,
+             :list_position, :notes_present,
              :happens_at, :completed_at,
              :latitude, :longitude,
              :created_at, :updated_at
 
-  def task_note_count
-    object.task_notes.size
+  def notes_present
+    return false if object.task_notes.blank?
+
+    object.task_notes.any? { |note| note.content.present? }
   end
 end
